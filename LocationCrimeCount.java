1baf7870891bd7ad2219a9f7cd9be3f9166ab9ff
shiva2735 <107064220+shiva2735@users.noreply.github.com>
2023-07-07 06:14:27 +0530

    Add files via upload

---
+++ LocationCrimeCount.java
@@ -0,0 +1,17 @@
+public class LocationCrimeCount {
+    private String location;
+    private int crimeCount;
+
+    public LocationCrimeCount(String location, int crimeCount) {
+        this.location = location;
+        this.crimeCount = crimeCount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCrimeCount() {
+        return crimeCount;
+    }
+}
